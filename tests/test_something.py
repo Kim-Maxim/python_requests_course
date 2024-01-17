c46e58750a5579863297e2258ab2a6240242a230
Maxim Kim <kimmaxim69@gmail.com>
2024-01-17 16:14:42 +0300

    Усложили задачу. Добавили первый реквест

---
+++ tests/test_something.py
@@ -0,0 +1,11 @@
+import requests
+
+from configuration import SERVICE_URL
+from src.enums.global_enums import GlobalErrorMessages
+
+def test_getting_posts():
+    response = requests.get(url=SERVICE_URL)
+    received_posts = response.json()
+
+    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
+    assert len(received_posts) == 3,  GlobalErrorMessages.WRONG_ELEMENT_COUNT
