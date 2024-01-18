e7506d7bfcd001294a995bed22053722cad15adc
Maxim Kim <kimmaxim69@gmail.com>
2024-01-18 17:51:56 +0300

    Улучшили валидацию объектов и добавили jsonschema

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,11 +1,11 @@
 import requests
 
+from src.baseclasses.response import Response
 from configuration import SERVICE_URL
-from src.enums.global_enums import GlobalErrorMessages
+from src.schemas.post import POST_SCHEMA
+
 
 def test_getting_posts():
-    response = requests.get(url=SERVICE_URL)
-    received_posts = response.json()
-
-    assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
-    assert len(received_posts) == 3,  GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+    response.assert_status_code(200).validate(POST_SCHEMA)
